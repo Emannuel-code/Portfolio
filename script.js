675b40c9597b12f21347e4c88a1ba3263e7cd220
manel.dev <114677770+Emannuel-code@users.noreply.github.com>
2023-02-08 21:34:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+///***MOBILE-BTN***///
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+         event.currentTarget.setAttribute('arialabel', 'Fechar menu');
+    } else {
+        event.currentTarget.setAttribute('arialabel', 'Abrir menu');
+    }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+///***SCROLL-REVEAL***///
+
 window.sr = ScrollReveal({ reset:true});
 ScrollReveal({ distance: '15px' });
 sr.reveal('.conteudo-principal', {duration : 2000, origin: 'right'})
@@ -6,6 +28,8 @@ sr.reveal('.projetos-itens', {duration : 2000, origin:'left'})
 sr.reveal('.projeto-descricao', {duration : 2000, origin:'right'})
 sr.reveal('.skills', {duration : 2000, origin:'left'})
 sr.reveal('.button', {duration : 2000, origin: 'left'})
+
+///***SCROLL-UP***///
 
 $(document).ready(function(){
     $(window).scroll(function(){    
